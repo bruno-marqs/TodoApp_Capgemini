be871b5bdd89693ed0da62415d753cec008b459e
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2022-11-06 13:09:09 -0300

    criando conexão db

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+// import java.sql.Connection;
+// import util.ConnectionFactory;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
---
+++ src/util/ConnectionFactory.java
@@ -0,0 +1,34 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    //127.0.0.1
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/to_do_app";
+    public static final String USER = "root";
+    public static final String PASS = "eds169910@@";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados");
+        }
+    }
+    
+    public static void closeConnection(Connection connection){
+        try {
+            if (connection != null){
+                connection.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar conexão com o banco de dados");
+        }
+    }
+
+}
